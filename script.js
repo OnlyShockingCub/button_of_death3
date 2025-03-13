ff5b5d57163d4636aa40006f267162d84ec8a4dc
OnlyShockingCub <165097100+OnlyShockingCub@users.noreply.github.com>
2025-03-13 16:30:41 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function openWindows() {
-    window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
+    window.open('https://onlyshockingcub.github.io/button_of_death2');
 }
 
 window.onload = function() {
